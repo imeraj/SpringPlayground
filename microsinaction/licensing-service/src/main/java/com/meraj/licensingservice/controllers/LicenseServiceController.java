d0e8bb5b7d0d6dd0039d16baf2a6949e33dbc2e8
Meraj <meraj.enigma@gmail.com>
2018-08-05 10:36:36 +0900

    Spring Microservices in Action
    microsinaction: Initial commit

---
+++ microsinaction/licensing-service/src/main/java/com/meraj/licensingservice/controllers/LicenseServiceController.java
@@ -0,0 +1,18 @@
+package com.meraj.licensingservice.controllers;
+
+import com.meraj.licensingservice.model.License;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+    @RequestMapping(value="/{licenseId}", method = RequestMethod.GET)
+    public License getLicenses(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        return new License(licenseId, "Telco", "Seat", organizationId);
+    }
+}
