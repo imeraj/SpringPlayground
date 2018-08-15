8f71cd96985fc6175be9c1916d5f68c443508bcc
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2018-08-15 21:24:53 +0900

    Hystrix and fallback method implemented in licensing service
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- microsinaction/licensing-service/src/main/java/com/meraj/licensingservice/LicensingServiceApplication.java
+++ microsinaction/licensing-service/src/main/java/com/meraj/licensingservice/LicensingServiceApplication.java
@@ -1,8 +1,8 @@
 package com.meraj.licensingservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class LicensingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LicensingServiceApplication.class, args);
--- microsinaction/licensing-service/src/main/java/com/meraj/licensingservice/clients/OrganizationDiscoveryClient.java
+++ microsinaction/licensing-service/src/main/java/com/meraj/licensingservice/clients/OrganizationDiscoveryClient.java
@@ -1,6 +1,8 @@
 package com.meraj.licensingservice.clients;
 
 import com.meraj.licensingservice.models.Organization;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -17,19 +19,27 @@ public class OrganizationDiscoveryClient {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @HystrixCommand(fallbackMethod = "buildFallbackOrganization")
     public Organization getOrganization(String organizationId) {
         RestTemplate restTemplate = new RestTemplate();
         List<ServiceInstance> instances = discoveryClient.getInstances("organizationservice");
 
-        if (instances.size()==0)
-            return null;
-        String serviceUri = String.format("%s/v1/organizations/%s",instances.get(0).getUri().toString(), organizationId);
+        String serviceUri = String.format("%s/v1/organizations/%s", instances.get(0).getUri().toString(), organizationId);
 
         ResponseEntity<Organization> restExchange
                 = restTemplate.exchange(serviceUri, HttpMethod.GET,null, Organization.class, organizationId);
 
-        System.out.println("BODY: " + restExchange.getBody());
-
         return restExchange.getBody();
+    }
+
+    private Organization buildFallbackOrganization(String organizationId) {
+        Organization org = new Organization();
+        org.setId("0000-0000-0000-0000");
+        org.setName("Meraj");
+        org.setContactName("Meraj");
+        org.setContactEmail("Meraj@gmail.com");
+        org.setContactPhone("000-0000-0000");
+
+        return org;
     }
 }
