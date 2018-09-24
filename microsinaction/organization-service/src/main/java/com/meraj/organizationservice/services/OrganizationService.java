478a0242f24287904c6b43e0b68033debcda82c5
Meraj <meraj.enigma@gmail.com>
2018-09-24 19:23:58 +0900

    Protecting a service by a specific role
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- microsinaction/organization-service/src/main/java/com/meraj/organizationservice/services/OrganizationService.java
+++ microsinaction/organization-service/src/main/java/com/meraj/organizationservice/services/OrganizationService.java
@@ -28,8 +28,8 @@ public class OrganizationService {
         orgRepository.save(org);
     }
 
-    public void deleteOrg(Organization org) {
-        orgRepository.deleteById(org.getId());
+    public void deleteOrg(String Id) {
+        orgRepository.deleteById(Id);
     }
 
 }
